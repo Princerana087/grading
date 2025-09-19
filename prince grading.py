f9fab71f95550ead7f63df4388a80f341cc9e3f5
Princerana087 <princerahul805112@gmail.com>
2025-09-19 19:37:45 +0530

    Add files via upload

---
+++ prince grading.py
@@ -0,0 +1,11 @@
+def grade(marks):
+    if marks>=90:
+        print("A")
+    elif marks>=80:
+        print("B")
+    elif marks>=70:
+        print("c")
+    else:
+     print("D")
+    
+grade(78)
